0f2ab0f93f13b339b7e45a46379a38991b794635
Gabriel Hackett <gabrielhackett15@gmail.com>
2025-08-25 21:01:15 -0400

    Fix postcss.config.js merge conflict

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-    // postcss.config.js
-    module.exports = {
-      plugins: [
-        require('autoprefixer'),
-        require('postcss-nested'),
-        // Add other plugins here
-      ],
-=======
-    // postcss.config.js
-    module.exports = {
-      plugins: [
-        require('autoprefixer'),
-        require('postcss-nested'),
-        // Add other plugins here
-      ],
->>>>>>> 70dcc887e8acb9bd607146bf63ca0cfe2d568a3e
-    };
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
