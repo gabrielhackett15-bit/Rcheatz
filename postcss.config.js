d4ef10cdc6eb34131e318cde44b060a51d600e89
gabrielhackett15-bit <gabrielhackett15@gmail.com>
2025-08-25 20:06:52 -0400

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+    // postcss.config.js
+    module.exports = {
+      plugins: [
+        require('autoprefixer'),
+        require('postcss-nested'),
+        // Add other plugins here
+      ],
+    };
